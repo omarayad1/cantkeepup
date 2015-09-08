0cd383bb1f61bd8e09e80c4fe02f04ce2c450099
ghooo <ghooo.ghooo@gmail.com>
2015-09-08 06:13:19 +0300

    adding ocvs keyword to search in opencv library in github

--- main.py
+++ main.py
@@ -5,9 +5,16 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     if 'q' in request.args:
-        return redirect("http://www.google.com/search?q="+request.args.get('q'), code=302)
+        return redirect(query(request.args.get('q')), code=302)
     else:
         return "Under Construction"
+
+def query(str):
+	if str.split(' ', 1)[0] == "ocvs":
+		return "https://github.com/Itseez/opencv/search?q=\"" + \
+		str.split(' ', 1)[1] + "\""
+	else:
+		return "http://www.google.com/search?q="+str
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
