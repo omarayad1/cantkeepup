bdec9f59a88ab86aee3a08cb8699a76d79f0c72e
ghooo <ghooo.ghooo@gmail.com>
2015-10-10 22:36:05 +0200

    fixing a bug

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,10 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
 from app.models import command, user, group
-from app.controllers import index
+from app.home import index
 
