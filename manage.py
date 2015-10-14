ca326c48c81a0879d9bd1b99690c5e410fdebc29
ghooo <ghooo.ghooo@gmail.com>
2015-10-14 16:17:58 +0200

    fixing a problem in heroku

--- manage.py
+++ manage.py
@@ -38,5 +38,10 @@ def cov():
     cov.html_report(directory=covdir)
     cov.erase()
 
+@manager.command
+def run():
+    port = int(os.environ.get('PORT', 5000))       
+    app.run(host='0.0.0.0', port=port)         
+
 if __name__ == '__main__':
     manager.run()
