8139748cee12823ab3df83a5cfd3cd75978092e8
ghooo <ghooo.ghooo@gmail.com>
2015-10-16 15:08:06 +0200

    moving the logic of handling queries to core.commandprocessor

--- app/core/commandprocessor.py
+++ app/core/commandprocessor.py
@@ -1,4 +1,9 @@
+import urllib # pragma: no cover
+from app.models import Command
 class CommandProcessor:
+	def __init__(self,command):
+		self.command = command
+
 	def parseCommand(self):
 		pass
 	
@@ -6,4 +11,21 @@ class CommandProcessor:
 		pass
 
 	def processCommand(self):
-		pass
+		tokens = self.command.split(' ', 1)
+
+		cmd_id = tokens[0]
+
+		if len(tokens) > 1:
+			commandText = tokens[1]
+			commandText = urllib.quote(commandText.encode('utf8'), safe='')
+		else:
+			commandText = ""
+
+		item = Command.query.filter_by(cmd_id=cmd_id).first()
+
+		if item is None:
+			self.command = urllib.quote(self.command.encode('utf8'), safe='')
+			return 'http://www.google.com/search?q=%s' % self.command
+		else:
+			data = item.url
+			return data.replace('%s', commandText)
--- app/home/views.py
+++ app/home/views.py
@@ -2,7 +2,7 @@ from flask.views import View # pragma: no cover
 from flask import request, redirect, Blueprint, render_template \
 		# pragma: no cover
 from app import models, app # pragma: no cover
-import urllib # pragma: no cover
+from app.core.commandprocessor import CommandProcessor
 
 home_blueprint = Blueprint(
 	'home', __name__,
@@ -13,26 +13,7 @@ home_blueprint = Blueprint(
 @home_blueprint.route('/') # pragma: no cover
 def home():
 	if 'q' in request.args:
-		query = request.args.get('q')
-
-		tokens = query.split(' ', 1)
-
-		cmd_id = tokens[0]
-
-		if len(tokens) > 1:
-			queryText = tokens[1]
-			queryText = urllib.quote(queryText.encode('utf8'), safe='')
-		else:
-			queryText = ""
-
-		item = models.command.Command.query.filter_by(cmd_id=cmd_id).first()
-
-		if item is None:
-			query = urllib.quote(query.encode('utf8'), safe='')
-			return redirect('http://www.google.com/search?q=%s' % \
-					query, code=302)
-		else:
-			data = item.url
-			return redirect(data.replace('%s', queryText), code=302)
+		commandProcessor = CommandProcessor(request.args.get('q'))
+		return redirect(commandProcessor.processCommand(),code=302)
 	else:
 		return render_template('index.html')
