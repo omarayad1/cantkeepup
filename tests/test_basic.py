ddbdf639c4b63c62c7bc23234f865b216efa2078
ghooo <ghooo.ghooo@gmail.com>
2015-10-16 14:47:58 +0200

    adding a description for the project in home page

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -9,7 +9,7 @@ class BasicTestCase(BaseTestCase):
 	# Ensure that home page opens correctly
 	def test_home(self):
 		response = self.client.get('/', content_type='html/text')
-		self.assertIn(b'Under Construction', response.data)
+		self.assertIn(b'Welcome to cantkeepup app', response.data)
 
 if __name__ == '__main__':
 	unittest.main()
